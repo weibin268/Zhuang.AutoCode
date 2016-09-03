e1af3c3384f08b3906d5d1e1ccc3b6773a7c8136
zhuangweibin <binweizhuang@hotmail.com>
2016-09-03 23:50:10 +0800

    aaa

---
+++ Zhuang.AutoCode/Zhuang.AutoCode.Test/EnvValStoreProviderTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zhuang.Data;
+
+namespace Zhuang.AutoCode.Test
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestClass]
+    public class EnvValStoreProviderTest
+    {
+        public EnvValStoreProviderTest()
+        {
+
+            Zhuang.Data.EnvironmentVariable.EnvValRepository.Instance.AddStoreProvider(new Common.EnvValStoreProvider());
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DbAccessor dba = DbAccessor.Get();
+            var str = dba.ExecuteScalar<string>("select '{{AutoCode:c}}'");
+
+            Console.WriteLine(str);
+        }
+
+
+    }
+}
---
+++ Zhuang.AutoCode/Zhuang.AutoCode/Common/EnvValStoreProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zhuang.Data.Common;
+
+namespace Zhuang.AutoCode.Common
+{
+    public class EnvValStoreProvider : Zhuang.Data.EnvironmentVariable.IEnvValStoreProvider
+    {
+        public Dictionary<string, object> GetEnvironmentVariables()
+        {
+            var dicResult = new Dictionary<string, object>();
+
+            dicResult.Add("AutoCode", new MyEnvFunc((c) =>
+            {
+                var autoCodeBuilder = new AutoCodeBuilder(c);
+
+                return autoCodeBuilder.Build();
+
+            }));
+
+            return dicResult;
+        }
+    }
+}
