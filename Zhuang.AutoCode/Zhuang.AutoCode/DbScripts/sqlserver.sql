06316e83e068a2be1250bffedfc69557e33a1568
zhuangweibin <binweizhuang@hotmail.com>
2016-11-28 16:35:13 +0800

    aaa

---
+++ Zhuang.AutoCode/Zhuang.AutoCode/DbScripts/sqlserver.sql
@@ -0,0 +1,22 @@
+﻿
+
+CREATE TABLE [dbo].[Sys_AutoCode] (
+    [AutoCodeId]  VARCHAR (50)   NOT NULL,
+    [Expression]  VARCHAR (500)  NOT NULL,
+    [Description] NVARCHAR (500) NULL
+);
+
+
+CREATE TABLE [dbo].[Sys_AutoCodeDetail] (
+    [AutoCodeDetailId] VARCHAR (50) NOT NULL,
+    [AutoCodeId]       VARCHAR (50) NOT NULL,
+    [PrefixCode]       VARCHAR (50) NULL,
+    [Seq]              INT          NOT NULL,
+    [CreatedDate]      DATETIME     NULL,
+    [ModifiedDate]     DATETIME     NULL
+);
+
+
+
+
+
